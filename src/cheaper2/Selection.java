e5ed9699bf29badbdd958cfd692f8e2510905141
palytoxin <sxty32@gmail.com>
2014-05-06 17:41:43 +0800

    In.readAllString ==> StdIn.readAllString.

--- src/cheaper2/Selection.java
+++ src/cheaper2/Selection.java
@@ -2,15 +2,14 @@ package cheaper2;
 
 import java.io.IOException;
 
-import stdlib.In;
+import stdlib.StdIn;
 
-
-public class Selection extends Example{
-	public static void sort(Comparable[] a){
+public class Selection extends Example {
+	public static void sort(Comparable[] a) {
 		int N = a.length;
 		for (int i = 0; i < N; i++) {
 			int min = i;// 后方成员最小的
-			for (int j = i+1; j < N; j++) {
+			for (int j = i + 1; j < N; j++) {
 				if (less(a[j], a[min])) {
 					min = j;
 				}
@@ -18,10 +17,12 @@ public class Selection extends Example{
 			exch(a, i, min);
 		}
 	}
+
 	public static void main(String[] args) throws IOException {
-		 String[] a = In.readStrings();
-		 sort(a);
-		 assert isSorted(a);
-		 show(a);
+		// please input Ctrl + z to send an EOF.
+		String[] a = StdIn.readAllStrings();
+		sort(a);
+		assert isSorted(a);
+		show(a);
 	}
 }
