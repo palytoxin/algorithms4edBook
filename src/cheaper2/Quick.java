a7cde665b7f531f04c69c407965a1756717be04a
Yan <sxty32@gmail.com>
2014-05-10 00:53:25 +0800

    quick, basic

---
+++ src/cheaper2/Quick.java
@@ -0,0 +1,58 @@
+package cheaper2;
+
+import java.util.Random;
+
+import stdlib.StdRandom;
+
+@SuppressWarnings("rawtypes")
+public class Quick extends Example {
+	public static void sort(Comparable[] a) {
+		StdRandom.shuffle(a);
+		sort(a, 0, a.length - 1);
+	}
+
+	private static void sort(Comparable[] a, int lo, int hi) {
+		if (hi <= lo)
+			return;
+		int j = partition(a, lo, hi);
+		sort(a, lo, j - 1);
+		sort(a, j + 1, hi); //  notice: var j
+	}
+
+	// 基本切分办法
+	private static int partition(Comparable[] a, int lo, int hi) {
+		int i = lo, j = hi + 1;
+		Comparable v = a[lo];
+		while (true) {
+			// 左查
+			while (less(a[++i], v))
+				if (i == hi)// 冗余判断
+					break;
+			// 右查
+			while (less(v, a[--j]))
+				if (j == lo)// 冗余判断
+					break;
+			// 除非 i/j 合一才去break
+			if (i >= j)
+				break;
+			exch(a, i, j);
+		}
+		// 交换切分元素，和左侧列表的最右元素
+		exch(a, lo, j);
+
+		return j;
+	}
+
+	public static void main(String[] args) {
+		Random r = new Random();
+		Integer[] a = new Integer[20];
+		for (int i = 0; i < a.length; i++) {
+			a[i] = r.nextInt(20);
+		}
+		show(a);
+
+		sort(a);
+		assert isSorted(a);
+		show(a);
+	}
+}
