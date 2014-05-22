afdfbba07bb946a7440c3a596dcb8691bc69a509
Yan <sxty32@gmail.com>
2014-05-22 21:57:05 +0800

    ..

---
+++ src/cheaper4/Bag.java
@@ -0,0 +1,17 @@
+package cheaper4;
+
+import java.util.Iterator;
+
+/**
+ * Created by yan on 14-5-22.
+ */
+public class Bag<T> implements Iterable {
+    @Override
+    public Iterator iterator() {
+        return null;
+    }
+
+    public void add(int i){
+
+    }
+}
---
+++ src/cheaper4/Graph.java
@@ -0,0 +1,48 @@
+package cheaper4;
+
+import stdlib.In;
+
+/**
+ * Created by yan on 14-5-22.
+ */
+public class Graph {
+    private final int V; // 顶点
+    private int E; //边数目
+    private Bag<Integer>[] adj; //邻接表
+
+    public Graph(int V) {
+        this.V = V;
+        this.E = 0;
+        adj = (Bag<Integer>[]) new Bag[V];
+        for (int v = 0; v < V; v++)
+            adj[v] = new Bag<Integer>();
+    }
+
+    public Graph(In in) {
+        this(in.readInt());
+        for (int i = 0; i < E; i++) {
+            int v = in.readInt();
+            int w = in.readInt();
+            addEdge(v, w);
+        }
+    }
+
+    public int V() {
+        return V;
+    }
+
+    public int E() {
+        return E;
+    }
+
+    public void addEdge(int v, int w) {
+        adj[v].add(w);
+        adj[w].add(v);
+        E++;
+    }
+
+    public Iterable<Integer> adj(int v) {
+        return adj[v];
+    }
+
+}
